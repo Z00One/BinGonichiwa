52c8b2ffa58eb0ab05a06fec0bdec2f29225ced7
Z00One <9000248@g.yju.ac.kr>
2023-11-12 22:21:00 +0900

    feat(app): Implement +bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&family=Orbitron:wght@400;600&family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet">
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <!-- Styles -->
+    @livewireStyles
+</head>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+<body>
+    <x-header>
+        <div class="relative w-full" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
+            <div @click="open = ! open">
+                <button type="button"
+                    class="inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                    <span class="text-2xl" :class="{ 'hidden': open }">+</span>
+                    <span class="text-xl" :class="{ 'hidden': !open }">x</span>
+                </button>
+            </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div x-show="open" @click="open = false">
+                <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white"
+                    style="position: absolute; left: -90px; top:0">
+
+                    <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                        href="http://127.0.0.1:8000/user/profile">Profile</a>
+
+                    <div class="border-t border-gray-200"></div>
+
+                    <form method="POST" action="http://127.0.0.1:8000/logout" x-data="">
+                        @csrf
+                        <a class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                            href="http://127.0.0.1:8000/logout" @click.prevent="$root.submit();">LogOut</a>
+                    </form>
+                </div>
+            </div>
         </div>
+    </x-header>
+    <main>
+        {{ $slot }}
+    </main>
+</body>
+@stack('modals')
 
-        @stack('modals')
+@livewireScripts
 
-        @livewireScripts
-    </body>
 </html>
