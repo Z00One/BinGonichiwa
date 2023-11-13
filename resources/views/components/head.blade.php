172d221f8bc21932d546d748008cbdb0a35e7399
Z00One <9000248@g.yju.ac.kr>
2023-11-13 19:41:02 +0900

    feat: Implement head tag as a component

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.svg') }}">
+    <title>{{ config('app.name', 'BinGonichiwa') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&family=Orbitron:wght@400;600&family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<style>
+    body {
+        margin: 0;
+    }
+</style>
