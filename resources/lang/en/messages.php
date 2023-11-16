4dc140ef5a9ed6e6f0a14059b2d89049d4fe6818
Z00One <9000248@g.yju.ac.kr>
2023-11-16 22:12:06 +0900

    feat: Add English language strings for user interface elements

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,44 @@
+<?php
+return [
+    'language' => 'Set To Jpn',
+    'logout' => 'Are you sure logout？',
+    'dashboard' => [
+        'first' => 'Welcome to BinGonichiwa.',
+        'second' => 'This service allows you to easily play bingo with other people.',
+        'third' => 'LogIn and enjoy.',
+    ],
+    'auth' => [
+        'error' => 'Whoops! Something went wrong.',
+        'register' => 'Register',
+        'already_registered' => 'Already registered?',
+    ],
+    'profile' => [
+        'saved' => 'Saved',
+        'save' => 'Save',
+        'update_information' => [
+            'title' => 'Profile Information',
+            'description' => 'Update your account\'s profile information and email address.',
+            'name' => 'Name',
+            'id' => 'ID',
+        ],
+        'update_password' => [
+            'title' => 'Update Password',
+            'description' => 'Ensure your account is using a long, random password to stay secure.',
+            'current_password' => 'Current Password',
+            'new_password' => 'New Password',
+            'confirm_password' => 'Confirm Password',
+        ],
+        'delete_account' => [
+            'title' => 'Delete Account',
+            'description' => 'Permanently delete your account.',
+            'content' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+            'delete_account_button' => 'Delete Account',
+            'cancel' => 'Cancel',
+            'modal' => [
+                'title' => 'Delete Account',
+                'content' => 'Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.',
+                'input_placehorder' => 'Password',
+            ]
+        ],
+    ],
+];
