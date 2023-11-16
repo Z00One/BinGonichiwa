a6c535de0f7a32c640817d0b4262a94500f815a0
Z00One <9000248@g.yju.ac.kr>
2023-11-16 22:13:08 +0900

    feat: Add Japanese language strings for user interface elements

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,53 @@
+<?php
+return [
+    'language' => 'Set To Eng',
+    'logout' => 'ログアウトしますか？',
+    'dashboard' => [
+        'first' => 'BinGonichiwaへようこそ。',
+        'second' => 'このサービスは他の人と簡単にビンゴゲームができます。',
+        'third' => 'ログインして楽しんでみませんか。',
+    ],
+    'auth' => [
+        'error' => '問題が発生しました。',
+        'register' => '登録',
+        'already_registered' => 'すでに登録済みです',
+    ],
+    'profile' => [
+        'saved' => '保存しました。',
+        'save' => '保存',
+        'update_information' => [
+            'title' => 'プロフィール情報',
+            'description' => 'アカウントのプロフィール情報とメールアドレスを更新してください。',
+            'name' => '名前',
+            'id' => 'ID',
+        ],
+        'update_password' => [
+            'title' => 'パスワードを更新',
+            'description' => 'アカウントが安全であるためには、長くランダムなパスワードを使用してください。',
+            'current_password' => '現在のパスワード',
+            'new_password' => '新しいパスワード',
+            'confirm_password' => 'パスワードの確認',
+        ],
+        'delete_account' => [
+            'title' => 'アカウントを削除',
+            'description' => 'アカウントを永久に削除します。',
+            'content' => 'アカウントを削除すると、すべてのリソースとデータが永久に削除されます。',
+            'modal' => [
+                'title' => 'アカウントを削除',
+                'content' => '本当にアカウントを削除しますか？ アカウントを削除すると、すべてのリソースとデータが永久に削除されます。 アカウントを永久に削除することを確認するには、パスワードを入力してください。',
+                'input_placehorder' => 'パスワード',
+                'cancel' => 'キャンセル',
+                'delete_account_button' => 'アカウントを削除',
+            ],
+        ],
+    ],
+];
+
+// messages.Set To Eng
+// __('BinGonichiwaへようこそ。') }}
+// </p>
+// <p class="mt-7 text-2xl font-medium text-myFontColor">
+//     {{ __('このサービスは他の人と簡単にビンゴゲームができます。') }}
+// </p>
+// <p class="mt-7 text-2xl font-medium text-myFontColor">
+//     {{ __('ログインして楽しんでみませんか。'
