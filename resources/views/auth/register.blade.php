663fb7018f53f9d37fcf79f2be5140ed16b7f158
Z00One <9000248@g.yju.ac.kr>
2023-11-17 20:42:45 +0900

    feat(register): Update registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,5 @@
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
         <x-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('register') }}">
@@ -16,9 +12,9 @@
             </div>
 
             <div class="mt-4">
-                <x-label for="id" value="{{ __('ID') }}" />
-                <x-input id="id" class="block mt-1 w-full" type="text" name="id" :value="old('id')"
-                    required autocomplete="username" />
+                <x-label for="email" value="{{ __('ID') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')"
+                    required autocomplete="email" />
             </div>
 
             <div class="mt-4">
@@ -27,46 +23,20 @@
                     autocomplete="new-password" />
             </div>
 
-            {{-- <div class="mt-4">
+            <div class="mt-4">
                 <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div> --}}
-
-            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
-                <div class="mt-4">
-                    <x-label for="terms">
-                        <div class="flex items-center">
-                            <x-checkbox name="terms" id="terms" required />
-
-                            <div class="ms-2">
-                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                    'terms_of_service' =>
-                                        '<a target="_blank" href="' .
-                                        route('terms.show') .
-                                        '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' .
-                                        __('Terms of Service') .
-                                        '</a>',
-                                    'privacy_policy' =>
-                                        '<a target="_blank" href="' .
-                                        route('policy.show') .
-                                        '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' .
-                                        __('Privacy Policy') .
-                                        '</a>',
-                                ]) !!}
-                            </div>
-                        </div>
-                    </x-label>
-                </div>
-            @endif
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                    name="password_confirmation" required autocomplete="new-password" />
+            </div>
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                <a class="underline text-sm text-gray-200 hover:text-myGreen rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('messages.auth.already_registered') }}
                 </a>
 
                 <x-button class="ms-4">
-                    {{ __('Register') }}
+                    Register
                 </x-button>
             </div>
         </form>
