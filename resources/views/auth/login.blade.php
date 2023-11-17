a7705384453351bc6cff9ba872da7d5814b2b3da
Z00One <9000248@g.yju.ac.kr>
2023-11-17 20:43:16 +0900

    fix(login): Fix ID label in login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,5 @@
 <x-guest-layout>
     <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
-
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
@@ -15,15 +11,10 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div>
-                <x-label for="id" value="{{ __('ID') }}" />
-                <x-input id="id" class="block mt-1 w-full" type="text" name="id" id="id"
-                    :value="old('id')" required autofocus />
+                <x-label for="email" value="{{ __('ID') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('id')" required
+                    autofocus autocomplete="username" />
             </div>
-            {{-- <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    required autofocus autocomplete="username" />
-            </div> --}}
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
@@ -38,16 +29,12 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
+            <div class="flex items-center justify-end mt-2">
+                <a href="{{ route('register') }}"
+                    class="text-gray-200 hover:text-myGreen transition ease-in-out duration-150">
+                    {{ __('messages.auth.register') }}</a>
                 <x-button class="ms-4">
-                    {{ __('Log in') }}
+                    LogIn
                 </x-button>
             </div>
         </form>
