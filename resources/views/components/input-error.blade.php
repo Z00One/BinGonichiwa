96b8d11b0579f549f5639f61343f799f2f2be75e
Z00One <9000248@g.yju.ac.kr>
2023-11-22 22:09:01 +0900

    fix(components): update input-error component

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,5 +1,11 @@
 @props(['for'])
 
 @error($for)
-    <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>{{ $message }}</p>
+    <p {{ $attributes->merge(['class' => 'text-sm text-red-600']) }}>
+        @if ($message === 'This password does not match our records.')
+            {{ __('messages.auth.wrong') }}
+        @else
+            {{ $message }}
+        @endif
+    </p>
 @enderror
