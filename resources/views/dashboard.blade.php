eb1f73b9fc10256054dd627590b136a383caa38e
Z00One <9000248@g.yju.ac.kr>
2023-11-22 22:07:08 +0900

    fix(dashboard): Fix user records count and winning rate calculation

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,45 +1,42 @@
 <x-app-layout>
-    <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="min-height: 90vh">
-        <div class="w-full sm:w-screen-lg py-2 mt-16 overflow-hidden">
-            <div class="text-center leading-10 px-5">
-                @auth
-                    @php
-                        $currentUser = Auth::user();
-                    @endphp
-                    <p class="text-2xl font-medium text-myFontColor">
-                        {{ $currentUser->name }}{{ __('messages.dashboard.record') }}
-                    </p>
-                    <p class="mt-7 text-2xl font-medium text-myFontColor">
-                        {{ $currentUser->records()->count() }} {{ __('messages.dashboard.games') }}
-                        {{ $currentUser->records()->where('is_win', true)->count() }} {{ __('messages.dashboard.wins') }}
-                        {{ $currentUser->records()->where('is_win', false)->count() }} {{ __('messages.dashboard.loses') }}
-                    </p>
-                    <div class="mt-10">
-                        <a href="">
-                            <x-button>
-                                {{ __('messages.dashboard.match') }}
-                            </x-button>
-                        </a>
-                    </div>
-                @else
-                    <p class="text-2xl font-medium text-myFontColor">
-                        {{ __('messages.dashboard.first') }}
-                    </p>
-                    <p class="mt-7 text-2xl font-medium text-myFontColor">
-                        {{ __('messages.dashboard.second') }}
-                    </p>
-                    <p class="mt-7 text-2xl font-medium text-myFontColor">
-                        {{ __('messages.dashboard.third') }} 😊
-                    </p>
-                    <div class="mt-10">
-                        <a href="{{ route('login') }}">
-                            <x-button>
-                                {{ __('messages.auth.login') }}
-                            </x-button>
-                        </a>
-                    </div>
-                @endauth
+    <div class="text-center leading-10 px-5">
+        @auth
+            @php
+                $user = Auth::user();
+                $winCount = $user->wins()->count();
+                $loseCount = $user->loses()->count();
+                $winningRate = ($winCount / ($winCount + $loseCount)) * 100 . '%';
+            @endphp
+            <p class="mt-7 text-2xl font-medium text-myFontColor">
+                {{ __('messages.dashboard.winning_rate', ['name' => $user->name]) }}
+            </p>
+            <p class="mt-5 text-2xl font-medium text-myFontColor">
+                {{ $winningRate }}
+            </p>
+            <div class="mt-10">
+                <a href="">
+                    <x-button>
+                        {{ __('messages.dashboard.match') }}
+                    </x-button>
+                </a>
             </div>
-        </div>
+        @else
+            <p class="text-2xl font-medium text-myFontColor">
+                {{ __('messages.dashboard.first') }}
+            </p>
+            <p class="mt-7 text-2xl font-medium text-myFontColor">
+                {{ __('messages.dashboard.second') }}
+            </p>
+            <p class="mt-7 text-2xl font-medium text-myFontColor">
+                {{ __('messages.dashboard.third') }} 😊
+            </p>
+            <div class="mt-10">
+                <a href="{{ route('login') }}">
+                    <x-button>
+                        {{ __('messages.auth.login') }}
+                    </x-button>
+                </a>
+            </div>
+        @endauth
     </div>
 </x-app-layout>
