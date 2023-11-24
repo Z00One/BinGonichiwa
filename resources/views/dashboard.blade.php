62758f120daaf857859bfe9cbcc211c6d2c68038
Z00One <9000248@g.yju.ac.kr>
2023-11-24 11:42:08 +0900

    feat(dashboard): update dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="text-center leading-10 px-5">
+    <div class="text-center leading-10 p-5 text-lg sm:text-2xl mt-20 sm:mt-0 font-medium text-myFontColor">
         @auth
             @php
                 $user = Auth::user();
@@ -7,10 +7,10 @@
                 $loseCount = $user->loses()->count();
                 $winningRate = ($winCount / ($winCount + $loseCount)) * 100 . '%';
             @endphp
-            <p class="mt-7 text-2xl font-medium text-myFontColor">
+            <p class="mt-7">
                 {{ __('messages.dashboard.winning_rate', ['name' => $user->name]) }}
             </p>
-            <p class="mt-5 text-2xl font-medium text-myFontColor">
+            <p class="mt-5">
                 {{ $winningRate }}
             </p>
             <div class="mt-10">
@@ -21,15 +21,17 @@
                 </a>
             </div>
         @else
-            <p class="text-2xl font-medium text-myFontColor">
-                {{ __('messages.dashboard.first') }}
-            </p>
-            <p class="mt-7 text-2xl font-medium text-myFontColor">
-                {{ __('messages.dashboard.second') }}
-            </p>
-            <p class="mt-7 text-2xl font-medium text-myFontColor">
-                {{ __('messages.dashboard.third') }} 😊
-            </p>
+            <div>
+                <p>
+                    {{ __('messages.dashboard.first') }}
+                </p>
+                <p class="mt-7">
+                    {{ __('messages.dashboard.second') }}
+                </p>
+                <p class="mt-7">
+                    {{ __('messages.dashboard.third') }} 😊
+                </p>
+            </div>
             <div class="mt-10">
                 <a href="{{ route('login') }}">
                     <x-button>
