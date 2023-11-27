61735855c70fb0e142ec3c22e4d48225fe2a5a98
Z00One <9000248@g.yju.ac.kr>
2023-11-27 23:26:32 +0900

    feat(GameController): Add functionality to retrieve channel list and assign user to a room

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,15 +5,99 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Game;
 use App\Models\User;
+use Illuminate\Support\Facades\Redis;
+use App\Events\WaitingEvent;
+use Illuminate\Support\Facades\Auth;
 
 class GameController extends Controller
 {
     /**
+     * Retrieves the channel list and assigns a user to a room.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        // 채널 리스트 가져오기
+        $channels = Redis::smembers(config('broadcasting.game.channels'));
+        $userId = Auth::user()->id;
+        $channel = '';
+
+        if (count($channels)) {
+            $findRoom = false;
+            
+            for ($i = 0; $i < count($channels) && !$findRoom; $i++) {
+                $channel = $channels[$i];
+                $room = Redis::hgetall($channel);
+                
+                if ($room == null) {
+                    Redis::hmset($channel, [$userId]);
+                    $findRoom = true;
+                }
+                
+                elseif (count($room) < 2) {
+                    array_push($room, $userId);
+                    Redis::hmset($channel, $room);
+                    $findRoom = true;
+                }
+
+                else {
+                    foreach ($room as $id) {
+                        if ($id === (string)$userId) {
+                            $findRoom = true;
+                        }
+                    }
+                }
+            }
+
+            if (!$findRoom) {
+                $channel = config('broadcasting.game.game') . bin2hex(random_bytes(5));
+                Redis::sadd(config('broadcasting.game.channels'), $channel);
+                Redis::hmset($channel, [$userId]);
+            }
+
+            broadcast(new WaitingEvent([
+                'channel' => $channel,
+                ]));
+        } 
+        else {
+            $channel = config('broadcasting.game.game') . bin2hex(random_bytes(5));
+            Redis::sadd(config('broadcasting.game.channels'), $channel);
+            Redis::hmset($channel, [$userId]);
+        }
+
+        return view('waiting', [
+            'channel' => $channel,
+            ]);
+    }
+
+    /**
+     * Leave the channel.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function leave()
+    {
+        $userId = Auth::user()->id;
+        $channelName = request()->input('channel');
+        $channel = Redis::hgetall($channelName);
+        
+        foreach ($channel as $user => $id) {
+            if ($id === (string)$userId) {
+                Redis::hdel($channelName, $user);
+                break;
+            }
+        }
+        
+        return redirect('/');
+    }
+    
+    /**
      * Show the form for creating the resource.
      */
-    public function create(): never
+    public function create(String $channel)
     {
-        abort(404);
+
     }
 
     /**
@@ -25,37 +109,11 @@ class GameController extends Controller
     }
 
     /**
-     * Display the resource.
+     * Retrieves the records of a user based on their name.
+     *
+     * @param string $name The name of the user.
+     * @return \Illuminate\View\View The view displaying the user's records.
      */
-    public function show()
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the resource.
-     */
-    public function edit()
-    {
-        //
-    }
-
-    /**
-     * Update the resource in storage.
-     */
-    public function update(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Remove the resource from storage.
-     */
-    public function destroy(): never
-    {
-        abort(404);
-    }
-
     public function record(String $name)
     {
         $user = User::where('name', $name)->first();
@@ -78,6 +136,7 @@ class GameController extends Controller
 
         $winCount = $user->wins()->count();
         $loseCount = $user->loses()->count();
+
         $winningRate = ($winCount + $loseCount) 
             ? ($winCount / ($winCount + $loseCount)) * 100
             : 0;
