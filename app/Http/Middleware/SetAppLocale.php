42209691d36eba886979d91598f81a9d0e6c855c
Z00One <9000248@g.yju.ac.kr>
2023-12-07 19:15:14 +0900

    feat(middleware): Implement language preference management

--- app/Http/Middleware/SetAppLocale.php
+++ app/Http/Middleware/SetAppLocale.php
@@ -5,17 +5,34 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\App;
 
-class SetAppLocale
+class SetAppLocale 
 {
+    /**
+     * Handle an incoming request.
+     *
+     * This middleware manages the user's language preference. 
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  ...$guards
+     * @return mixed
+     */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-      $previousLocale = Session::get('locale');
+        $locale = $request->route('lang');
 
-      $locale = $previousLocale ?? request()->getPreferredLanguage(['ja', 'en']);
+        $supportedLanguages = config('app.supported_languages');
 
-      app()->setLocale($locale);
-      
-      return $next($request);
+        if (in_array($locale, $supportedLanguages)) {
+            Session::put('locale', $locale);
+        } else {
+            $locale = Session::get('locale', App::getLocale());
+        }
+
+        app()->setLocale($locale);
+
+        return $next($request);
     }
 }
