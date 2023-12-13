f02a60ea982debecaf7b59c1369d0911adc8ad7d
Z00One <9000248@g.yju.ac.kr>
2023-12-13 19:08:04 +0900

    style(app): update layout in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,12 @@
 <body>
     @include('components.header')
 
-    <main>
-        {{ $slot }}
-    </main>
+    <div class="w-full sm:max-w-screen-lg mx-auto flex flex-col sm:justify-center items-center pt-6 sm:pt-0"
+        style="min-height: 92vh">
+        <div class="w-full sm:w-screen-lg py-2 overflow-hidden">
+            {{ $slot }}
+        </div>
+    </div>
 </body>
 
 @stack('modals')
