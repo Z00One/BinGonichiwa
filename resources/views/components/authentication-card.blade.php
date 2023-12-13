5124927d8cbbd3716b13d65e8a68efc1495a0b19
Z00One <9000248@g.yju.ac.kr>
2023-12-13 19:09:22 +0900

    style(authentication-card): update margin-top in authentication card component

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,5 +1,5 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-    <div class="w-full sm:max-w-md mt-20 px-6 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-20 sm:mt-0 px-6 py-2 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
