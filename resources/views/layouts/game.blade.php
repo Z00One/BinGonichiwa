aa2d796533bd016efa967be9e50df0eb00d75722
Z00One <9000248@g.yju.ac.kr>
2023-12-13 19:07:18 +0900

    feat(game): Update to use symentic tag

--- resources/views/layouts/game.blade.php
+++ resources/views/layouts/game.blade.php
@@ -2,11 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 @include('components.head')
-<script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-{{-- <script src="{{ asset('resources/js/echo-config.js') }}"></script> --}}
 
 <body>
-    <div class="absolute top-0 left-0 w-full">
+    <header class="absolute top-0 left-0 w-full">
         <div class="max-w-screen-lg mx-auto p-2">
             <div class="flex items-center">
                 <img src="{{ asset('assets/favicon.svg') }}" class="h-8" alt="favicon">
@@ -14,14 +12,13 @@
             </div>
         </div>
         <hr />
-    </div>
+    </header>
     <div class="max-w-screen-lg mx-auto flex flex-col sm:justify-center items-center pt-6 sm:pt-0"
         style="min-height: 90vh">
         <div class="w-full sm:w-screen-lg py-2 overflow-hidden">
             {{ $slot }}
         </div>
     </div>
-
 </body>
 
 </html>
